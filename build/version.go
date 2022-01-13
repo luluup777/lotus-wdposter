188222f5e08ba90c78f3be5e0b50872b18d422e8
luluup777 <luluup777@163.com>
2022-01-13 10:14:39 +0800

    add version

---
+++ build/version.go
@@ -0,0 +1,14 @@
+package build
+
+const (
+	wdKeepManagerVersion = "v0.0.1"
+	wdKeepWorkerVersion = "v0.0.1"
+)
+
+func WdKeepManagerAPIVersion() string {
+	return wdKeepManagerVersion
+}
+
+func WdKeepWorkerAPIVersion() string {
+	return wdKeepWorkerVersion
+}
